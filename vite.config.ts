9a9b19417d7b088f57177ceaf4ca0a80ba3ab008
Leonardo Chappuis <leonardochappuis@gmail.com>
2025-06-16 21:31:05 -0300

    First commit

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import tailwindcss from "@tailwindcss/vite";
+import react from '@vitejs/plugin-react-swc'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    tailwindcss(),
+    react(),
+    // Add any additional plugins here
+  ],
+})
